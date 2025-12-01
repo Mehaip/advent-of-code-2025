e97ebc8ac06bf7db491f4a0eabdca657adf7fc6c
Mihai Buga <mihai.buga.bv@gmail.com>
2025-12-01 16:54:16 +0200

    pb1 gata

--- pb1/1.py
+++ pb1/1.py
@@ -4,23 +4,22 @@ def solve():
     line = file.readline()
     password = 0
     scor = 50
-    iteration = 1
     file_out.write("Miscare,Scor,Password\n")
     file_out.write("-,50,0\n")
     while line:
         shift_direction = line[0]
         shift_number2 = (int)(line[1:])
-        password = password + shift_number2 // 100
+        password = password + (shift_number2 // 100)
         shift_number = shift_number2 % 100
         if shift_direction == 'L':
             if scor == 0:
                 scor = 100 - shift_number
-            if scor - shift_number > 0:
+            elif scor - shift_number > 0:
                 scor = scor - shift_number
-            if scor - shift_number < 0:
+            elif scor - shift_number < 0:
                 password += 1
                 scor = 100 + (scor - shift_number)
-            if scor - shift_number == 0:
+            elif scor - shift_number == 0:
                 scor = 0
                 password += 1
         else:
@@ -31,7 +30,6 @@ def solve():
                 password += 1
         file_out.write(f"{shift_direction}{shift_number2},{scor},{password}\n")
         line = file.readline()
-        iteration += 1
 
         
     print(password)
